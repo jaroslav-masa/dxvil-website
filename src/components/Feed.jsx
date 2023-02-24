8f75c772f7c141a8be22af8bd2ba5606216471a5
dxvil-exe <musicbyblakk@gmail.com>
2023-02-24 15:13:34 +0100

    Edited text

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,7 +6,7 @@ const Feed = () => {
   return (
     <Stack sx={{display: "grid", textAlign: "center", placeItems: "center", height: "100vh"}} className="bigText">
       <Box sx={{fontSize: "4.5rem"}}>
-        Meet the future
+        Meet the future of music
       </Box>
     </Stack>
   )

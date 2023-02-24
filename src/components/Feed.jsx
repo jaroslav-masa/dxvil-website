9b7b7d40340b6f62c27599854fccab47300b7d02
dxvil-exe <musicbyblakk@gmail.com>
2023-02-24 15:35:54 +0100

    added Glitch effect

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,8 +5,8 @@ import React from 'react'
 const Feed = () => {
   return (
     <Stack sx={{display: "grid", textAlign: "center", placeItems: "center", height: "100vh"}} className="bigText">
-      <Box sx={{fontSize: "4.5rem"}}>
-        Meet the future of music
+      <Box sx={{fontSize: "4.5rem"}} className="glitchContainer">
+        <h4 className="glitch mainlayer layers" data-text="Meet the future of music"><span>Meet the future of music</span></h4>
       </Box>
     </Stack>
   )

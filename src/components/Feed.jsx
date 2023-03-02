e09b0c56594ee56ab5179814c6b10119ef029de1
dxvil-exe <musicbyblakk@gmail.com>
2023-03-02 15:59:40 +0100

    added calling Spotify API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,13 @@
 import { Stack } from '@mui/system'
 import React from 'react'
-import { BigTitle, AboutSection, ContactSection, Footer } from '.'
+import { BigTitle, AboutSection, ContactSection, Footer, TrackList } from '.'
 
 
 const Feed = () => {
   return (
     <Stack sx={{maxHeight: "87.5vh", overflowY: "auto"}}>
       <BigTitle/>
+      <TrackList/>
       <AboutSection/>
       <ContactSection/>
       <Footer/>
